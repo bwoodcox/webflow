ceef81d98e7be80b34fad2855a1d32a00c7f633c
Corey Woodcox <corey.woodcox@gmail.com>
2012-02-18 16:50:44 -0700

    move logic to webflow

--- lib/webflow.rb
+++ lib/webflow.rb
@@ -1,5 +1,18 @@
 require "webflow/version"
 
+# Load the basic stuff
+require 'webflow/exceptions'
+require 'webflow/base'
+require 'webflow/session_handler'
+require 'webflow/state'
+require 'webflow/event'
+require 'webflow/random_generator'
+require 'webflow/flow_step'
+require 'webflow/action_step'
+require 'webflow/view_step'
+#require 'webflow/plugin'
+
 module WebFlow
-  # Your code goes here...
+  # Reserve the render user event for the Base class.
+  WebFlow::Base.reserve_event :render
 end
